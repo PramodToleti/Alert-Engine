319af00a52ed2cc26ae16d65e11bbe4b61d59536
PramodToleti <pramodraina047@gmail.com>
2023-10-18 17:08:59 +0530

    Added Edit policy route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,18 @@ import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Policies from "./components/Policies"
 import PolicyForm from "./components/PolicyForm"
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Toaster />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/policies" element={<Policies />} />
         <Route path="/policies/new" element={<PolicyForm />} />
+        <Route path="/policies/edit/:id" element={<PolicyForm />} />
         {/* <Route path="/alerts" element={<Alerts />} /> */}
       </Routes>
     </BrowserRouter>
